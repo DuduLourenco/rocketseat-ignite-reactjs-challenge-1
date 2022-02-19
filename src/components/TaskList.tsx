91d3123de5319fb11ce45fbd60e102894bf46c1c
Eduardo Lourenço <eduardo.silva@hubchain.com>
2022-02-19 19:35:03 -0300

    [:tada:] Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,34 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle.trim()) {
+      return;
+    }
+
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 100000),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks([...tasks, newTask]);
+    setNewTaskTitle("");
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskToUpdate = tasks.find(task => task.id === id);
+
+    if(taskToUpdate) {
+      taskToUpdate.isComplete = !taskToUpdate.isComplete;
+
+      setTasks([...tasks]);
+    }
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTaskArray = tasks.filter(task => task.id !== id);
+
+    setTasks(newTaskArray);
   }
 
   return (
